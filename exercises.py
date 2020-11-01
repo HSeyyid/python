953e3729dd73bd270aa0b8e0ba0948a86b96a83f
kuscu44 <32467338+kuscu44@users.noreply.github.com>
2020-11-01 17:33:21 +0300

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -67,3 +67,14 @@ divbythree = ["Yes" if number % 3 == 0 else "No" for number in range(1,20)]
 print(divbythree)
 
 ### Exercise 6 ###
+#Write your function here
+def delete_starting_evens(lst):
+  while len(lst) > 0 and lst[0] % 2 == 0:
+    lst.pop(0)
+    if len(lst) == 0:
+      return []
+  return lst
+
+#Uncomment the lines below when your function is done
+print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
+print(delete_starting_evens([4, 8, 10]))
